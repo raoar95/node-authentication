8d1755351644164ea965327cf281477082e45091
Arun Rao <yarun1995.ar@gmail.com>
2024-12-25 22:31:09 +0530

    Constant Updated

--- src/constant/constant.js
+++ src/constant/constant.js
@@ -1,4 +1,2 @@
 export const DB_NAME = "VideoTube";
-export const SERVER_URL =
-  process.env.SERVER_URL ||
-  "mongodb+srv://myStorage:myStorage54123@mystorage.flqri.mongodb.net";
+export const SERVER_URL = process.env.SERVER_URL;
