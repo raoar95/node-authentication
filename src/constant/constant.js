ab14d7ff09512c573622a8690582288ada0fe4bd
Arun Rao <yarun1995.ar@gmail.com>
2025-02-21 02:10:17 +0530

    Constant Updated

--- src/constant/constant.js
+++ src/constant/constant.js
@@ -5,12 +5,13 @@ export const SERVER_URL = process.env.MONGODB_URI;
 // Environment
 export const NODE_ENV = process.env.NODE_ENV;
 
-// Cors
-export const CORS_ORIGIN = process.env.CORS_ORIGIN;
-
 // Host
-export const PRO_SERVER = process.env.PRO_SERVER;
-export const LOCAL_SERVER = `http://localhost:${PORT}`;
+export const PROD_SERVER = process.env.PROD_SERVER;
+export const LOCAL_SERVER = process.env.LOCAL_SERVER;
+
+// Cors
+export const CORS_ORIGIN =
+  process.env.NODE_ENV === "development" ? LOCAL_SERVER : PROD_SERVER;
 
 // Email Configuration
 export const EMAIL = process.env.EMAIL;
